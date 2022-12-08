2554bb7e8b7e928f580a770a683f69c2f6cf690b
yusufzq <65050809+yusufzq@users.noreply.github.com>
2022-12-08 16:05:16 +0000

    add font

--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,21 @@
 import Document, { Html as HTML, Head, Main, NextScript } from 'next/document';
 
 class Document2 extends Document {
-    render() {
-        return (
-            <HTML>
-                <Head />
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </HTML>
-        );
-    };
+	render() {
+		return (
+			<HTML>
+				<Head>
+					<link rel='preload' as='font' href='/fonts/IBMPlexSans-Regular.ttf' crossOrigin='anonymous' />
+					<link rel='preload' as='font' href='/fonts/IBMPlexSans-SemiBold.ttf' crossOrigin='anonymous' />
+					<link rel='preload' as='font' href='/fonts/IBMPlexSans-Bold.ttf' crossOrigin='anonymous' />
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</HTML>
+		);
+	};
 };
 
 export default Document2;
