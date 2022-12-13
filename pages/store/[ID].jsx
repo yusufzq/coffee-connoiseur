f8a746ebfb7613e7e4e4ca4fa40a601ebc2b491e
yusufzq <65050809+yusufzq@users.noreply.github.com>
2022-12-13 14:58:50 +0000

    develop store page, add icons and styles

--- pages/store/[ID].jsx
+++ pages/store/[ID].jsx
@@ -1,6 +1,10 @@
 import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
+import classNames from 'classnames';
 import storesData from '../../data/stores.json';
+import styles from '../../styles/store.module.css';
 
 export function getStaticPaths() {
 	const paths = storesData.map(({ ID }) => ({
@@ -21,19 +25,48 @@ export async function getStaticProps({ params }) {
 	};
 };
 
-const Store = ({ name }) => {
+const Store = ({ name, imageURL, address, neighbourhood }) => {
 	const router = useRouter();
 	
 	if (router.isFallback) {
 		return <h1>Loading...</h1>
 	};
 	
+	const onUpVoteButtonClick = () => {
+		console.log('CLICK');
+	};
+	
 	return (
 		<>
-		<Head>
-			<title>{name}</title>
-		</Head>
-			<p>{name}</p>
+			<Head>
+				<title>{name}</title>
+			</Head>
+			<main className={classNames(styles.layout, styles.container)}>
+				<section className={styles.col1}>
+					<div className={styles.backToHomeLink}>
+						<Link href='/'>Home</Link>
+					</div>
+					<div className={styles.nameWrapper}>
+						<h1 className={styles.name}>{name}</h1>
+					</div>
+					<Image src={imageURL} alt={name} className={styles.storeImg} width={600} height={360} />
+				</section>
+				<section className={classNames('glass', styles.col2)}>
+					<div className={styles.iconWrapper}>
+						<Image src='/icons/places.svg' alt='places icon' width={24} height={24} />
+						<p className={styles.text}>{address}</p>
+					</div>
+					<div className={styles.iconWrapper}>
+						<Image src='/icons/locationArrow.svg' alt='location arrow icon' width={24} height={24} />
+						<p className={styles.text}>{neighbourhood}</p>
+					</div>
+					<div className={styles.iconWrapper}>
+						<Image src='/icons/star.svg' alt='star icon' width={24} height={24} />
+						<p className={styles.text}>7</p>
+					</div>
+					<button className={styles.upvoteButton} onClick={onUpVoteButtonClick}>UpVote</button>
+				</section>
+			</main>
 		</>
 	);
 };
