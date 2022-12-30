9da587fff41007ebeffc7b263a2d2ca269229e77
yusufzq <65050809+yusufzq@users.noreply.github.com>
2022-12-30 15:09:43 +0000

    make shop context and reducer

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,48 @@
+import { createContext, useReducer } from 'react';
 import '../styles/globals.css';
 
 function Application({ Component, pageProps }) {
-	return <Component {...pageProps} />
+	const ShopContext = createContext();
+
+	const ShopProvider = ({ children }) => {
+		const initialState = {
+			coordinates: '',
+			shops: []
+		};
+
+		const reducer = (state, { type, payload }) => {
+			switch (type) {
+				case 'SET_COORDINATES':
+					return {
+						...state,
+						coordinates: payload.coordinates
+					};
+					
+				case 'SET_SHOPS':
+					return {
+						...state,
+						shops: payload.shops
+					};
+
+				default:
+					throw new Error(`InCorrect Action Type: ${type}`);
+			};
+		};
+
+		const [ state, dispatch ] = useReducer(reducer, initialState);
+
+		return (
+			<ShopContext.Provider value={{ state, dispatch }}>
+				{children}
+			</ShopContext.Provider>
+		);
+	};
+
+	return (
+		<ShopProvider>
+			<Component {...pageProps} />
+		</ShopProvider>
+	);
 };
 
 export default Application;
