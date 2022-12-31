2f1d86b4742197284672b7aa5e4f8f6835424a6d
yusufzq <65050809+yusufzq@users.noreply.github.com>
2022-12-31 20:18:54 +0000

    fetch AirTable record with POST

--- pages/api/shops.js
+++ pages/api/shops.js
@@ -1,15 +1,45 @@
 // Next API Route Support: https://nextjs.org/docs/api-routes/introduction
 
+import airTable from '../../lib/airtable';
 import { shopsGet } from '../../srv/shops';
 
 async function shops(request, response) {
-	try {
-		const { coordinates, limit } = request.query;
-		const shops = await shopsGet(limit, coordinates);
+	switch (request.method) {
+		case 'GET': {
+			try {
+				const { coordinates, limit } = request.query;
+				const shops = await shopsGet(limit, coordinates);
+		
+				response.status(200).json(shops);
+			} catch (error) {
+				response.status(500).send(`500 Internal Server Error: ${error}`);
+			};
+		};
 
-		response.status(200).json(shops);
-	} catch (error) {
-		response.status(500).send(`Internal Server Error (500): ${error}`);
+			break;
+
+		case 'POST': {
+			try {
+				const shopRecords = await airTable
+					.select({filterByFormula: 'ID="0"'})
+					.firstPage();
+
+				if (shopRecords.length !== 0) {
+					const records = shopRecords.map(record => ({...record.fields}));
+
+					response.status(200).json(records);
+				} else {
+					// create record
+				};
+			} catch (error) {
+				response.status(500).send(`500 Internal Server Error: ${error}`);
+			};
+		};
+
+			break;
+		
+		default:
+			break;	
 	};
 };
 
