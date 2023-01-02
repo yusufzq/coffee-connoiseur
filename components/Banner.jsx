cfa700987a4e90a7f5c0372e7903b0a24c0fced9
yusufzq <65050809+yusufzq@users.noreply.github.com>
2023-01-02 13:01:50 +0000

    simplify banner

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,21 +1,19 @@
 import styles from '../styles/Banner.module.css';
 
-const Banner = ({ buttonText, onButtonClick }) => {
-	return (
-		<header className={styles.container}>
-			<hgroup>
-				<h1 className={styles.title}>
-					<span className={styles.title1}>Coffee</span>
-					&nbsp;
-					<span className={styles.title2}>Connoisseur</span>
-				</h1>
-				<p className={styles.subTitle}>Discover Local Coffee Shops</p>
-			</hgroup>
-			<div className={styles.buttonWrapper}>
-				<button className={styles.button} onClick={onButtonClick}>{buttonText}</button>
-			</div>
-		</header>
-	);
-};
+const Banner = ({ buttonText, onButtonClick }) => (
+	<header className={styles.container}>
+		<hgroup>
+			<h1 className={styles.title}>
+				<span className={styles.title1}>Coffee</span>
+				&nbsp;
+				<span className={styles.title2}>Connoisseur</span>
+			</h1>
+			<p className={styles.subTitle}>Discover Local Coffee Shops</p>
+		</hgroup>
+		<div className={styles.buttonWrapper}>
+			<button className={styles.button} onClick={onButtonClick}>{buttonText}</button>
+		</div>
+	</header>
+);
 
 export default Banner;
