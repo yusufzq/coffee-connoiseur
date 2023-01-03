914d93fd9dfe60f0dee9ca1ccbff234e181a7bf6
yusufzq <65050809+yusufzq@users.noreply.github.com>
2023-01-03 15:45:30 +0000

    remove title from document

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -7,7 +7,6 @@ class Document2 extends Document {
 				<Head>
 					<meta name='title' content='Coffee Connoisseur' />
 					<meta name='description' content='Search for Coffee Shops NearBy' />
-					<title>Coffee Connoisseur</title>
 					<link rel='icon' type='image/x-icon' href='/favicon.ico' />
 					<link rel='preload' as='font' href='/fonts/IBMPlexSans-Regular.ttf' crossOrigin='anonymous' />
 					<link rel='preload' as='font' href='/fonts/IBMPlexSans-SemiBold.ttf' crossOrigin='anonymous' />
